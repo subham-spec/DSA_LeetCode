6b1825827510fdcc1712b96d1be7bdb6185340a5
Subham Sharma <subhamsharma@Subhams-MacBook-Pro.local>
2023-07-07 12:15:26 +0530

    tree

---
+++ Binary Trees/BottomViewOfBT.cpp
@@ -0,0 +1,37 @@
+#include "Node.cpp"
+#include <iostream>
+#include <map>
+#include <queue>
+using namespace std;
+
+vector <int> bottomView(Node *root) {
+        // Your Code Here
+        vector<int> ans;
+        if(!root)
+            return ans;
+            
+        map<int, int> mp;
+        queue<pair<int, Node*>> q;
+        q.push({0, root});
+    
+        while(!q.empty()) {
+            int size = q.size();
+            for(int i=0; i<size; i++) {
+                Node* node = q.front().second;
+                int idx = q.front().first;
+                
+                q.pop();
+                mp[idx] = node->data;
+                
+                if(node->left)
+                    q.push({idx-1, node->left});
+                if(node->right)
+                    q.push({idx+1, node->right});
+            }
+        }
+        
+        for(auto i: mp) {
+            ans.push_back(i.second);
+        }
+        return ans;
+    }
